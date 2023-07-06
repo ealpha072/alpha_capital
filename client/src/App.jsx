20f7c6df79752a85a1a01e5dca332ff3bb9302a6
ealpha072 <ealpha072@gmail.com>
2023-07-06 10:45:00 +0300

    Folder structures

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Header from './Components/Header'
+import Footer from './Components/Footer'
 
 const App = () => {
   return (
     <div>
-      <h1>Welcome</h1>
+      <Header />
+      <Footer />
     </div>
   )
 }
