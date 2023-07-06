d573dc37e2e1c261e495bc4b26f2af34f0b6b20c
ealpha072 <ealpha072@gmail.com>
2023-07-06 10:45:00 +0300

    More folders

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import logo from '../Assets/logo.png'
+//import {Link} from 'react-router-dom'
+
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+        <nav>
+            <div>
+                <div>
+                    <img src={logo} alt="" width={"60"}/>
+                </div>
+                <div>
+                    <a href="#">HOME</a>
+                    <a href="#">ABOUT US</a>
+                    <a href="">SERVICES</a>
+                    <a href="">CONTACT US</a>
+                    <a href="">CAREERS</a>
+                </div>
+                <div></div>
+            </div>
+        </nav>
+    </header>
   )
 }
 
