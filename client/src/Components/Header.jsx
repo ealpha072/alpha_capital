7bc63218db62c74f7071926dca1b2003b1d22e77
ealpha072 <ealpha072@gmail.com>
2023-07-07 10:50:00 +0300

    Module error fix

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,24 +1,26 @@
 import React from 'react'
 import logo from '../Assets/logo.png'
+import styles from '../Styles/Header.module.css'
 //import {Link} from 'react-router-dom'
 
 
 const Header = () => {
   return (
     <header>
-        <nav>
+        <nav >
             <div>
-                <div>
-                    <img src={logo} alt="" width={"60"}/>
+                <div className={styles.logo}>
+                    <img src={logo} alt="" width={"70"}/>
                 </div>
-                <div>
+                <div className={styles.actions}>
                     <a href="#">HOME</a>
                     <a href="#">ABOUT US</a>
                     <a href="">SERVICES</a>
                     <a href="">CONTACT US</a>
-                    <a href="">CAREERS</a>
                 </div>
-                <div></div>
+                <div className={styles.academy}>
+                    <a href="">ACADEMY</a>
+                </div>
             </div>
         </nav>
     </header>
