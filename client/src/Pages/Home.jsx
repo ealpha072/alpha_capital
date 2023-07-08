ab4b68ae41a0bd694c48df8b4c15109cf1cdc6d9
ealpha072 <ealpha072@gmail.com>
2023-07-08 10:50:00 +0300

    Background image fix

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
+import styles from '../Styles/Home.module.css'
 
 const Home = () => {
     return (
-        <div>
+        <div className={styles.homeDiv}>
             <div>
-                <div></div>
-                <div></div>
+                <div className={styles.backgroundDiv}>
+                    <div className={styles.blurOverlay}>
+                        <p>CREATIVE SOLUTIONS FOR EVERYONE</p>
+                        <h1>Get help with data analysis from experts. </h1>
+                        <h2>DATASEAL SOFTWARE transforms how businesses work and people live 
+                            through the power of data. We help businesses across AFRICA turn 
+                            data into their greatest asset.</h2>
+                        <div className="bannerBtns">
+                            <a href=""></a>
+                            <a href=""></a>
+                        </div>
+                    </div>
+                </div>
             </div>
             
 
