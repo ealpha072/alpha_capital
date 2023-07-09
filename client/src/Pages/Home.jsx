d2d5efdcb494e7cd99b3a49204805812cb9d3307
ealpha072 <ealpha072@gmail.com>
2023-07-09 10:50:00 +0300

    Image styles

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -7,14 +7,18 @@ const Home = () => {
             <div>
                 <div className={styles.backgroundDiv}>
                     <div className={styles.blurOverlay}>
-                        <p>CREATIVE SOLUTIONS FOR EVERYONE</p>
-                        <h1>Get help with data analysis from experts. </h1>
-                        <h2>DATASEAL SOFTWARE transforms how businesses work and people live 
-                            through the power of data. We help businesses across AFRICA turn 
-                            data into their greatest asset.</h2>
-                        <div className="bannerBtns">
-                            <a href=""></a>
-                            <a href=""></a>
+                        <div>
+                            <h6>CREATIVE SOLUTIONS FOR EVERYONE</h6>
+                            <h1>Get help with data analysis from experts. </h1>
+                            <p>
+                                ALPHA CAPITAL transforms how businesses work and people live 
+                                through the power of data. We help businesses across AFRICA turn 
+                                data into their greatest asset.
+                            </p>
+                            <div className="bannerBtns">
+                                <a href=""></a>
+                                <a href=""></a>
+                            </div>
                         </div>
                     </div>
                 </div>
